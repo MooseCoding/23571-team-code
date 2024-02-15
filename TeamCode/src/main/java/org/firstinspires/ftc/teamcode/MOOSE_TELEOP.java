30708a856406a12b9b2b0ebed791f59b42d76cee
MooseCoding <113293872+MooseCoding@users.noreply.github.com>
2024-02-15 11:10:13 -0500

    Create MOOSE_TELEOP.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MOOSE_TELEOP.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+public class MOOSE_TELEOP extends LinearOpMode {
+  @Override
+  public void runOpMode() {
+    while(isOpModeActive && !isStopRequested) {
+      
+    }
+  }
+}
